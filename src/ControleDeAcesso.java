0e0586721b43e6ae5e7dde51f180ea847d361b44
Gugdit <gugudittricht@gmail.com>
2024-11-05 14:21:33 -0300

    Projeto de Controle de Acesso SENAI

--- src/ControleDeAcesso.java
+++ src/ControleDeAcesso.java
@@ -88,10 +88,12 @@ public class ControleDeAcesso {
     }
 
     private static void exibirCadastro() {
-        String tabela = "";
+        StringBuilder tabela = new StringBuilder();
+
         for (int linhas = 0; linhas < matrizCadastro.length; linhas++) {
             for (int colunas = 0; colunas < matrizCadastro[0].length; colunas++) {
-                tabela += matrizCadastro[linhas][colunas];
+                tabela.append(matrizCadastro[linhas][colunas]);
+
                 tabela += colunas == 0 ? "\t" : "\t\t\t";
             }
             tabela += "\n";
